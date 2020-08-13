da21db3e4d8c9c3ec8bab5b3d81eea9e28e31527
Michel <michcodes@hotmail.com>
2020-08-13 12:33:13 -0500

    Pomodoro Start

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
 function App() {
-  return <p>dom render desde App component</p>
+  return (
+    <div className="app">
+      <h2>Pomodoro App</h2>
+
+      <div className="timer">
+        <span>00</span>
+        <span>:</span>
+        <span>00</span>
+      </div>
+
+      <div className="buttons">
+        {<button>Comienza</button> || <button>Pausa</button>}
+        <button>Reset</button>
+      </div>
+    </div>
+  )
 }
 
 export default App
